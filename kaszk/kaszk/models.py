84d5522dfe6d891c9a6a2acbdd97081005449bf3
antal-komornik <komornik.anti@gmail.com>
2023-02-21 15:13:29 +0100

    hírek feltültése

--- kaszk/kaszk/models.py
+++ kaszk/kaszk/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class News(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=255)
+    group = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.group}-{self.title}-{self.date}"
--- kaszk/kaszk/views.py
+++ kaszk/kaszk/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . models import News
+from . form import NewsForm
 # Create your views here.
 
 
 def Home(requset):
-    return render(requset, "index.html")
+    news = News.objects.all()
+    return render(requset, "index.html", {"news": news})
+
+
+def newNews(request):
+    if request.method == "GET":
+        form = NewsForm()
+        return render(request, "newNews.html", {"form": form})
+    elif request.method == "POST":
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
